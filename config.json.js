3c3f1d8c800a418ba4567f547c9405caf81ab5bb
wengheng <wengheng@everstu.com>
2023-01-28 15:15:54 +0800

    up:v1.4.2 回退二进制至v3.8.0

--- config.json.js
+++ config.json.js
@@ -3,9 +3,9 @@
     "description": "Alist. 一个支持多种存储的文件列表程序，使用 Gin 和 Solidjs", 
     "home_url": "Module_alist.asp", 
     "link": "https://github.com/everstu/Koolcenter_alist", 
-    "md5": "91f7145d2af9172d90833a10c7d6fa60", 
+    "md5": "61bef87b36c5a1eb9a661b574965256f", 
     "module": "alist", 
     "tags": "云同步", 
     "title": "Alist 文件列表", 
-    "version": "1.4.1"
+    "version": "1.4.2"
 }
