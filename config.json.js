1caed2b220bbe9121ae25ab8afc99906c7ab3abc
everstu <whtony2003@163.com>
2023-05-29 09:17:22 +0800

    up:v1.7.1

--- config.json.js
+++ config.json.js
@@ -3,9 +3,9 @@
     "description": "Alist. 一个支持多种存储的文件列表程序，使用 Gin 和 Solidjs", 
     "home_url": "Module_alist.asp", 
     "link": "https://github.com/everstu/Koolcenter_alist", 
-    "md5": "d88d5c6b28edb836d501e06c8d8fc006", 
+    "md5": "d311770a4f8b1d9b1d897cb630dec4c4", 
     "module": "alist", 
     "tags": "云同步", 
     "title": "Alist 文件列表", 
-    "version": "1.7"
+    "version": "1.7.1"
 }
