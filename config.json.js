3a8c8159a5f843c2beb69b8e084906bcb7e27da0
WengHeng <whtony2003@163.com>
2024-07-22 11:08:38 +0800

    bump bin ver 3.36

--- config.json.js
+++ config.json.js
@@ -3,9 +3,9 @@
     "description": "Alist. 一个支持多种存储的文件列表程序，使用 Gin 和 Solidjs", 
     "home_url": "Module_alist.asp", 
     "link": "https://github.com/everstu/Koolcenter_alist", 
-    "md5": "9dbb0ecd85a574aea434146556745971", 
+    "md5": "cc0443e922f70f887cdf7ac4dd4ac36f", 
     "module": "alist", 
     "tags": "云同步", 
     "title": "Alist 文件列表", 
-    "version": "2.0.1"
+    "version": "2.0.2"
 }
