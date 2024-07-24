c7f6e71fadcfff08a12d200a6b9cc5a25976d2d6
WengHeng <whtony2003@163.com>
2024-07-24 12:01:54 +0800

    fix:临时目录无法正常使用的问题

--- config.json.js
+++ config.json.js
@@ -3,7 +3,7 @@
     "description": "Alist. 一个支持多种存储的文件列表程序，使用 Gin 和 Solidjs", 
     "home_url": "Module_alist.asp", 
     "link": "https://github.com/everstu/Koolcenter_alist", 
-    "md5": "67df448af78e7a83d0c558b1010b4109", 
+    "md5": "a8bf38c7549db4fd32e014c40699b0b6", 
     "module": "alist", 
     "tags": "云同步", 
     "title": "Alist 文件列表", 
