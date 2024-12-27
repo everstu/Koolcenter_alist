d66e509f004aa816f04bc66e8d509846603c941d
WengHeng <wengheng@lifebanktech.com>
2024-12-27 10:59:09 +0800

    up:v2.2.1

--- config.json.js
+++ config.json.js
@@ -3,9 +3,9 @@
     "description": "Alist. 一个支持多种存储的文件列表程序，使用 Gin 和 Solidjs", 
     "home_url": "Module_alist.asp", 
     "link": "https://github.com/everstu/Koolcenter_alist", 
-    "md5": "dcba61fc535b1a88a7ed1c18ffa8d87f", 
+    "md5": "4c62da0c8aafdc62ce4b23aafafc9d65", 
     "module": "alist", 
     "tags": "云同步", 
     "title": "Alist 文件列表", 
-    "version": "2.2.0"
+    "version": "2.2.1"
 }
